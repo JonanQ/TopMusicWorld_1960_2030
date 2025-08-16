75d137a913a1f4c81b0829b738f252a1fe00aec2
JonanQ <jonanq@gmail.com>
2025-08-16 18:39:16 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,44 +1,100 @@
 // === Datos (30 temas) con género ===
-const songs = [
-  // 1980s
-  { id: "Zi_XLOBDo_Y", title: "Billie Jean - Michael Jackson – 1980s", genre: "Pop / R&B" },
-  { id: "djV11Xbc914", title: "Take On Me - a-ha – 1980s", genre: "Synth-pop / New wave" },
-  { id: "OMOGaugKpzs", title: "Every Breath You Take - The Police – 1980s", genre: "New wave / Pop rock" },
-  { id: "1w7OgIMMRc4", title: "Sweet Child O' Mine - Guns N' Roses – 1980s", genre: "Hard rock" },
-  { id: "lDK9QqIzhwk", title: "Livin' on a Prayer - Bon Jovi – 1980s", genre: "Hard rock / Glam metal" },
-  { id: "FTQbiNvZqaY", title: "Africa - Toto – 1980s", genre: "Soft rock / Pop rock" },
+const ukSongs = [
+  // 1960s
+  {
+    title: "Yesterday",
+    artist: "The Beatles",
+    decade: "60s",
+    year: 1965,
+    genre: "Pop rock",
+    youtube: "https://www.youtube.com/watch?v=NrgmdOz227I",
+    cover: "https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_yesterday_and_today.jpg"
+  },
+  {
+    title: "(I Can’t Get No) Satisfaction",
+    artist: "The Rolling Stones",
+    decade: "60s",
+    year: 1965,
+    genre: "Rock",
+    youtube: "https://www.youtube.com/watch?v=nrIPxlFzDi0",
+    cover: "https://upload.wikimedia.org/wikipedia/en/b/b2/Satisfaction-us.jpg"
+  },
+  {
+    title: "My Generation",
+    artist: "The Who",
+    decade: "60s",
+    year: 1965,
+    genre: "Rock",
+    youtube: "https://www.youtube.com/watch?v=qN5zw04WxCc",
+    cover: "https://upload.wikimedia.org/wikipedia/en/d/de/TheWhoMyGeneration.jpg"
+  },
+  {
+    title: "Sunshine of Your Love",
+    artist: "Cream",
+    decade: "60s",
+    year: 1967,
+    genre: "Blues rock",
+    youtube: "https://www.youtube.com/watch?v=zt51rITH3EA",
+    cover: "https://upload.wikimedia.org/wikipedia/en/9/94/CreamDisraeliGears.jpg"
+  },
+  {
+    title: "Space Oddity",
+    artist: "David Bowie",
+    decade: "60s",
+    year: 1969,
+    genre: "Glam rock",
+    youtube: "https://www.youtube.com/watch?v=iYYRH4apXDo",
+    cover: "https://upload.wikimedia.org/wikipedia/en/1/12/David_Bowie_-_Space_Oddity.jpg"
+  },
 
-  // 1990s
-  { id: "bx1Bh8ZvH84", title: "Wonderwall - Oasis – 1990s", genre: "Britpop / Alternative rock" },
-  { id: "XFkzRNyygfk", title: "Creep - Radiohead – 1990s", genre: "Alternative rock" },
-  { id: "SSbBvKaM6sk", title: "Song 2 - Blur – 1990s", genre: "Alternative rock / Britpop" },
-  { id: "luwAMFcc2f8", title: "Angels - Robbie Williams – 1990s", genre: "Pop / Pop rock" },
-  { id: "gJLIiF15wjQ", title: "Wannabe - Spice Girls – 1990s", genre: "Pop / Dance-pop" },
-  { id: "1lyu1KKwC74", title: "Bitter Sweet Symphony - The Verve – 1990s", genre: "Alternative rock / Britpop" },
+  // 1970s
+  {
+    title: "Imagine",
+    artist: "John Lennon",
+    decade: "70s",
+    year: 1971,
+    genre: "Pop rock",
+    youtube: "https://www.youtube.com/watch?v=YkgkThdzX-8",
+    cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/JohnLennon-albums-imagine.jpg"
+  },
+  {
+    title: "Bohemian Rhapsody",
+    artist: "Queen",
+    decade: "70s",
+    year: 1975,
+    genre: "Progressive rock",
+    youtube: "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+    cover: "https://upload.wikimedia.org/wikipedia/en/9/9f/Bohemian_Rhapsody.png"
+  },
+  {
+    title: "Anarchy in the U.K.",
+    artist: "Sex Pistols",
+    decade: "70s",
+    year: 1976,
+    genre: "Punk",
+    youtube: "https://www.youtube.com/watch?v=qbmWs6Jf5dc",
+    cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Anarchy_in_the_UK.jpg"
+  },
+  {
+    title: "Heroes",
+    artist: "David Bowie",
+    decade: "70s",
+    year: 1977,
+    genre: "Rock",
+    youtube: "https://www.youtube.com/watch?v=lXgkuM2NhYI",
+    cover: "https://upload.wikimedia.org/wikipedia/en/6/60/Bowie_Heroes.jpg"
+  },
+  {
+    title: "Wuthering Heights",
+    artist: "Kate Bush",
+    decade: "70s",
+    year: 1978,
+    genre: "Art pop",
+    youtube: "https://www.youtube.com/watch?v=-1pMMIe4hb4",
+    cover: "https://upload.wikimedia.org/wikipedia/en/8/84/Wuthering_Heights_%28Kate_Bush_single_-_cover_art%29.jpg"
+  },
 
-  // 2000s
-  { id: "gGdGFtwCNBE", title: "Mr. Brightside - The Killers – 2000s", genre: "Indie rock / Post-punk revival" },
-  { id: "yKNxeF4KMsY", title: "Yellow - Coldplay – 2000s", genre: "Alternative rock / Post-Britpop" },
-  { id: "0J2QdDbelmY", title: "Seven Nation Army - The White Stripes – 2000s", genre: "Garage rock / Alternative rock" },
-  { id: "PWgvGjAhvIw", title: "Hey Ya! - OutKast – 2000s", genre: "Hip hop / Funk-pop" },
-  { id: "DUT5rEU6pqM", title: "Hips Don't Lie - Shakira ft. Wyclef Jean – 2000s", genre: "Latin pop / Reggaetón / Dance-pop" },
-  { id: "ViwtNLUqkMY", title: "Crazy In Love - Beyoncé ft. Jay-Z – 2000s", genre: "R&B / Pop" },
-
-  // 2010s
-  { id: "JGwWNGJdvx8", title: "Shape of You - Ed Sheeran – 2010s", genre: "Pop / Dancehall-pop" },
-  { id: "OPf0YbXqDm0", title: "Uptown Funk - Mark Ronson ft. Bruno Mars – 2010s", genre: "Funk-pop / Disco-pop" },
-  { id: "ZbZSe6N_BXs", title: "Happy - Pharrell Williams – 2010s", genre: "Soul / Funk-pop" },
-  { id: "rYEDA3JcQqw", title: "Rolling in the Deep - Adele – 2010s", genre: "Soul / Pop / Blues-pop" },
-  { id: "zABLecsR5UE", title: "Someone You Loved - Lewis Capaldi – 2010s", genre: "Pop / Ballad" },
-  { id: "4NRXx6U8ABQ", title: "Blinding Lights - The Weeknd – 2010s", genre: "Synth-wave / Synth-pop" },
-
-  // 2020s
-  { id: "H5v3kku4y6Q", title: "As It Was - Harry Styles – 2020s", genre: "Synth-pop / Indie pop" },
-  { id: "orJSJGHjBLI", title: "Bad Habits - Ed Sheeran – 2020s", genre: "Dance-pop" },
-  { id: "TUVcZfQe-Kw", title: "Levitating - Dua Lipa – 2020s", genre: "Disco-pop / Nu-disco" },
-  { id: "ZmDBbnmKpqQ", title: "drivers license - Olivia Rodrigo – 2020s", genre: "Pop / Bedroom pop" },
-  { id: "G7KNmW9a75Y", title: "Flowers - Miley Cyrus – 2020s", genre: "Pop / Disco-pop" },
-  { id: "LIIDh-qI9oI", title: "Save Your Tears (Remix) - The Weeknd & Ariana Grande – 2020s", genre: "Synth-pop" }
+  // … continuar con los 80s, 90s, 00s, 10s, 20s …
 ];
 
 // === Render + Filtro ===
