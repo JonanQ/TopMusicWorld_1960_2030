9c6782975abf3ba1fc3c4b7690ea2cb57c1506e5
JonanQ <jonanq@gmail.com>
2025-08-16 21:11:42 +0200

    Create uk-songs.js

---
+++ uk-songs.js
@@ -0,0 +1,97 @@
+const ukSongs = [
+  // 1960s
+  {
+    title: "Yesterday",
+    artist: "The Beatles",
+    decade: "60s",
+    year: 1965,
+    genre: "Pop rock",
+    youtube: "https://www.youtube.com/watch?v=NrgmdOz227I",
+    cover: "https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_yesterday_and_today.jpg"
+  },
+  {
+    title: "(I Can’t Get No) Satisfaction",
+    artist: "The Rolling Stones",
+    decade: "60s",
+    year: 1965,
+    genre: "Rock",
+    youtube: "https://www.youtube.com/watch?v=nrIPxlFzDi0",
+    cover: "https://upload.wikimedia.org/wikipedia/en/b/b2/Satisfaction-us.jpg"
+  },
+  {
+    title: "My Generation",
+    artist: "The Who",
+    decade: "60s",
+    year: 1965,
+    genre: "Rock",
+    youtube: "https://www.youtube.com/watch?v=qN5zw04WxCc",
+    cover: "https://upload.wikimedia.org/wikipedia/en/d/de/TheWhoMyGeneration.jpg"
+  },
+  {
+    title: "Sunshine of Your Love",
+    artist: "Cream",
+    decade: "60s",
+    year: 1967,
+    genre: "Blues rock",
+    youtube: "https://www.youtube.com/watch?v=zt51rITH3EA",
+    cover: "https://upload.wikimedia.org/wikipedia/en/9/94/CreamDisraeliGears.jpg"
+  },
+  {
+    title: "Space Oddity",
+    artist: "David Bowie",
+    decade: "60s",
+    year: 1969,
+    genre: "Glam rock",
+    youtube: "https://www.youtube.com/watch?v=iYYRH4apXDo",
+    cover: "https://upload.wikimedia.org/wikipedia/en/1/12/David_Bowie_-_Space_Oddity.jpg"
+  },
+
+  // 1970s
+  {
+    title: "Imagine",
+    artist: "John Lennon",
+    decade: "70s",
+    year: 1971,
+    genre: "Pop rock",
+    youtube: "https://www.youtube.com/watch?v=YkgkThdzX-8",
+    cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/JohnLennon-albums-imagine.jpg"
+  },
+  {
+    title: "Bohemian Rhapsody",
+    artist: "Queen",
+    decade: "70s",
+    year: 1975,
+    genre: "Progressive rock",
+    youtube: "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+    cover: "https://upload.wikimedia.org/wikipedia/en/9/9f/Bohemian_Rhapsody.png"
+  },
+  {
+    title: "Anarchy in the U.K.",
+    artist: "Sex Pistols",
+    decade: "70s",
+    year: 1976,
+    genre: "Punk",
+    youtube: "https://www.youtube.com/watch?v=qbmWs6Jf5dc",
+    cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Anarchy_in_the_UK.jpg"
+  },
+  {
+    title: "Heroes",
+    artist: "David Bowie",
+    decade: "70s",
+    year: 1977,
+    genre: "Rock",
+    youtube: "https://www.youtube.com/watch?v=lXgkuM2NhYI",
+    cover: "https://upload.wikimedia.org/wikipedia/en/6/60/Bowie_Heroes.jpg"
+  },
+  {
+    title: "Wuthering Heights",
+    artist: "Kate Bush",
+    decade: "70s",
+    year: 1978,
+    genre: "Art pop",
+    youtube: "https://www.youtube.com/watch?v=-1pMMIe4hb4",
+    cover: "https://upload.wikimedia.org/wikipedia/en/8/84/Wuthering_Heights_%28Kate_Bush_single_-_cover_art%29.jpg"
+  },
+
+  // … continuar con los 80s, 90s, 00s, 10s, 20s …
+];
